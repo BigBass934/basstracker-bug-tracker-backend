98baba350a8daab925ea8e233f919a7d5929679d
jam5322 <jam5322@g.rit.edu>
2021-03-06 21:26:36 -0500

    maybe a fix for credentials

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 const app = require('./app');
+const aws = require("aws-sdk");
+aws.config.getCredentials(function(err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    console.log("Access key:", aws.config.credentials.accessKeyId);
+}
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
